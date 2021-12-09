1b78e31d977981f281160902d0a48cf1d2ce4815
Zhiming <zhiminghuang@uvic.ca>
2021-12-08 16:42:18 -0800

    update
    update

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Ip_address: the ip address of where receiver runs
 Port_number: the port number is 8888 hardcoded in receiver.py
 required_loss: required loss rate
 packet_no: how many packets to be sent
+
+
+
+Assumption: For each video chunk, the sender will first transmit basic layer and then the enhancement layer
+For simplicity, we assume the basic layer requires 1 packet, each enhance layer requires 1 additional layer
+There are 3 quality (480p 720p 1080p), i.e., 480p (1 basic layer packet), 720p (1 basic layer packet + 1 enhancement layer), 1080p(1 basic layer packet + 2 enhancement layer) 
+Assume the network bandwith can allow 3 packets transmitted at the same time and packets are sent in a back-to-back way
+Assume the application always requests
---
+++ sim4mab.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 13:38:00 2021
+
+@author: zhiming
+"""
+import matplotlib
+from contex_mab import *
+import numpy as np
+
+
+drp_rate = 0.2
+
+#RTT =  150 assume one trip time is 75ms, and round trip time is 150ms
+num_seg = 5000
+
+#RTT =  np.random.uniform(140,160,num_seg)
+#drp = np.random.binomial(1,drp_rate,num_seg)
+
+# A basic-layer segment follows by a enhancement-layer segment
+packet_imp = 1
+
+# Assume every 200ms, two segment (i.e., basic and enhancement) with 200ms content is generated
+# and the delay requirement for this segment is 120 ms for each segment
+# the maximum snd_wnd is 2, i.e., at most two segments can be sent at a time
+snd_wnd = 2
+
+mabctl = MAB_Control()
+
+# Initially, we have two segments already generated
+seg_buffer = 2
+
+t = 0
+
+segment_spawn = np.zeros(num_seg) + 200
+segment_spawn[0] = 0
+seg_buffer = np.cumsum(segment_spawn)
+#Context: (delay_req, seg_importance, seg_sndbuffer, seg_bitrate, snd_wnd)
+
+
+for i in range(num_seg):
+    rtt =  np.random.uniform(140,160,num_seg)
+    mabctl.update_rtt(update_rtt)
+    #observe context
+    seg_buffer -= 2
+    delayReq = 
+    mabctl.input_context(delayReq, packet_imp, seg_buffer, snd_wnd)
+    packet_imp = -packet_imp
+    action1 = mabctl.exp3_action()
+    if action1 == 0:
+        delay = np.random.geometric(1-drp_rate)
+        
